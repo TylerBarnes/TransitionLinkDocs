fd774bc69b63ceaa91b748579f2b830222bcd09d
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-17 21:34:27 -0800

    add google fonts + formatting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,94 +1,103 @@
 const wordsbyConfig = {
-  siteName: "TransitionLink Docs",
-  shortName: "TransitionLink",
-  siteDescription: "A Wordsby site for gatsby-plugin-transition-link (late 2018)",
+  siteName: 'TransitionLink Docs',
+  shortName: 'TransitionLink',
+  siteDescription:
+    'A Wordsby site for gatsby-plugin-transition-link (late 2018)',
   url: {
-    base: "transitionlink.temperance.online",
-    protocol: "https",
-    pathPrefix: false
+    base: 'transitionlink.temperance.online',
+    protocol: 'https',
+    pathPrefix: false,
   },
   manifest: {
-    background_color: "#6b37bf",
-    theme_color: "#6b37bf",
-    display: "minimal-ui"
+    background_color: '#6b37bf',
+    theme_color: '#6b37bf',
+    display: 'minimal-ui',
   },
   keys: {
-    previewToken: "test",
-    googleAnalyticsID: false
-  }
-};
+    previewToken: 'test',
+    googleAnalyticsID: false,
+  },
+}
 
-const previewPrefix = require("wordsby/preview");
-const fullUrl = `${wordsbyConfig.url.protocol}://${wordsbyConfig.url.base}`;
+const previewPrefix = require('wordsby/preview')
+const fullUrl = `${wordsbyConfig.url.protocol}://${wordsbyConfig.url.base}`
 
 const gatsbyConfig = {
   pathPrefix: previewPrefix(), // if you need to add a prefix to this site, pass it as a string eg. previewPrefix("/some-prefix").
   siteMetadata: {
-    siteUrl: fullUrl
+    siteUrl: fullUrl,
   },
   plugins: [
     {
       resolve: `gatsby-plugin-emotion`,
       options: {
         // Accepts all options defined by `babel-plugin-emotion` plugin.
-      }
+      },
     },
     {
-      resolve: "gatsby-source-wordpress",
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono\:300,400,400i,700`, // you can also specify font weights and styles
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-source-wordpress',
       options: {
         name: wordsbyConfig.siteName,
         short_name: wordsbyConfig.shortName,
         start_url: wordsbyConfig.url.startUrl
           ? wordsbyConfig.url.startUrl
-          : "/",
+          : '/',
         background_color: wordsbyConfig.manifest.background_color,
         theme_color: wordsbyConfig.manifest.theme_color,
-        display: "minimal-ui",
-        icon: "src/favicon.png",
+        display: 'minimal-ui',
+        icon: 'src/favicon.png',
         baseUrl: wordsbyConfig.url.base,
         protocol: wordsbyConfig.url.protocol,
         useACF: false, // this should be false as the wordsby rest api endpoint has ACF built in.
         verboseOutput: false,
         includedRoutes: [
-          "**/wp-api-menus/v2/",
-          "**/wp-api-menus/v2/**",
-          "**/wp/v1/collections",
-          "**/wp/v2/taxonomies",
-          "**/wp/v2/media",
-          "**/wp/v2/media/(?P<id>[d]+)"
+          '**/wp-api-menus/v2/',
+          '**/wp-api-menus/v2/**',
+          '**/wp/v1/collections',
+          '**/wp/v2/taxonomies',
+          '**/wp/v2/media',
+          '**/wp/v2/media/(?P<id>[d]+)',
         ],
         searchAndReplaceContentUrls: {
           sourceUrl: `${fullUrl}(?!/wp-content/)`, // this replaces all urls except for media library links allowing us to use the WP permalink structure.
-          replacementUrl: ""
-        }
-      }
+          replacementUrl: '',
+        },
+      },
     },
     {
-      resolve: "wordsby",
+      resolve: 'wordsby',
       options: {
-        previewToken: wordsbyConfig.keys.previewToken
-      }
+        previewToken: wordsbyConfig.keys.previewToken,
+      },
     },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-lodash",
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-lodash',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "assets",
-        path: `${__dirname}/static/`
-      }
+        name: 'assets',
+        path: `${__dirname}/static/`,
+      },
     },
     {
-      resolve: "gatsby-plugin-nprogress",
+      resolve: 'gatsby-plugin-nprogress',
       options: {
-        color: wordsbyConfig.themeColor
-      }
+        color: wordsbyConfig.themeColor,
+      },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-catch-links",
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-catch-links',
     {
-      resolve: "gatsby-plugin-sitemap",
+      resolve: 'gatsby-plugin-sitemap',
       options: {
         query: `{
           site {
@@ -106,20 +115,26 @@ const gatsbyConfig = {
               }
             }
           }
-      }`
-      }
+      }`,
+      },
     },
-    "gatsby-plugin-offline"
-  ]
-};
+    {
+      resolve: 'gatsby-plugin-transition-link',
+      options: {
+        layout: require.resolve(`./src/layout/index.js`),
+      },
+    },
+    // 'gatsby-plugin-offline',
+  ],
+}
 
 if (wordsbyConfig.keys.googleAnalyticsID) {
   gatsbyConfig.plugins.push({
-    resolve: "gatsby-plugin-google-analytics",
+    resolve: 'gatsby-plugin-google-analytics',
     options: {
-      trackingId: wordsbyConfig.keys.googleAnalyticsID
-    }
-  });
+      trackingId: wordsbyConfig.keys.googleAnalyticsID,
+    },
+  })
 }
 
-module.exports = gatsbyConfig;
+module.exports = gatsbyConfig

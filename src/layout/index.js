87ed81abcc2850c82f59356b364d70d2833d6497
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-17 21:36:02 -0800

    add typography styles to the page

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,17 +1,21 @@
-import React from "react";
-import Helmet from "react-helmet";
-import config from "../../data/SiteConfig";
+import React from 'react'
+import Helmet from 'react-helmet'
+import { injectGlobal } from 'emotion'
+import * as theme from '../theme'
 
+injectGlobal`
+  ${theme.typographyString}
+`
 export default class MainLayout extends React.Component {
   render() {
-    const { children } = this.props;
+    const { children } = this.props
     return (
       <div>
         <Helmet>
-          <meta name="description" content={config.siteDescription} />
+          <meta name="description" />
         </Helmet>
         {children}
       </div>
-    );
+    )
   }
 }

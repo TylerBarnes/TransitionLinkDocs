235498060dd7aacf6bdf89b00b95be05f3ba4abd
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-24 12:36:53 -0800

    add active status to menu items

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,49 +1,65 @@
 import React from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
+import { LocationProvider } from '@reach/router'
 
 export default ({ slug: propSlug, children }) => {
   return propSlug ? (
-    <StaticQuery
-      query={graphql`
-        {
-          allWordpressWpApiMenusMenusItems {
-            edges {
-              node {
-                slug
-                items {
-                  title
-                  url
-                  wordpress_id
+    <LocationProvider>
+      {({ location: { pathname } }) => (
+        <StaticQuery
+          query={graphql`
+            {
+              allWordpressWpApiMenusMenusItems {
+                edges {
+                  node {
+                    slug
+                    items {
+                      title
+                      url
+                      wordpress_id
+                    }
+                  }
                 }
               }
             }
-          }
-        }
-      `}
-      render={({ allWordpressWpApiMenusMenusItems: wpmenu }) => {
-        const menu = wpmenu.edges.filter(({ node }) => node.slug === propSlug)
+          `}
+          render={({ allWordpressWpApiMenusMenusItems: wpmenu }) => {
+            const menu = wpmenu.edges.filter(
+              ({ node }) => node.slug === propSlug
+            )
 
-        const items = menu.length > 0 ? menu[0].node.items : false
+            let items = menu.length > 0 ? menu[0].node.items : false
 
-        if (children) {
-          return children(items)
-        }
+            if (items) {
+              items.forEach(item => {
+                item.active = item.url === pathname ? true : false
+              })
+            }
 
-        return items ? (
-          <div className="">
-            {items.map(item => (
-              <Link key={`menu-item-${item.wordpress_id}`} to={item.url}>
-                {item.title}
-              </Link>
-            ))}
-          </div>
-        ) : (
-          <h2>
-            {propSlug} doesn't return anything. Maybe you have a spelling error?
-          </h2>
-        )
-      }}
-    />
+            console.log(items)
+
+            if (children) {
+              return children(items)
+            }
+
+            return items ? (
+              <div className="">
+                {items.map(item => (
+                  <Link key={`menu-item-${item.wordpress_id}`} to={item.url}>
+                    {item.title}
+                  </Link>
+                ))}
+              </div>
+            ) : (
+              <h2>
+                {propSlug} doesn't return anything. Maybe you have a spelling
+                error?
+              </h2>
+            )
+          }}
+        />
+      )}
+    </LocationProvider>
   ) : (
     <h2>Add a WP menu slug to return menu items.</h2>
   )
